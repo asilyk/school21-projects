cb7f71bd1abbee7f7aebd955fce6107c42adbda5
Ferret Abet <fabet@ox-c3.msk.21-school.ru>
2021-10-10 13:50:55 +0300

    ft_calloc is protected

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: fabet <fabet@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 17:55:46 by fabet             #+#    #+#             */
-/*   Updated: 2021/10/08 18:09:39 by fabet            ###   ########.fr       */
+/*   Updated: 2021/10/10 13:44:38 by fabet            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*ptr;
 
 	ptr = (void *)malloc(count * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 	ft_bzero(ptr, count * size);
 	return (ptr);
 }
