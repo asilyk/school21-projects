0da186c7c664fca445dcd4bc4e4748451aa654e1
Alexander <alexander@MacBook-Pro.local>
2021-10-12 08:17:11 +0300

    ft_put*_fd are done

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fabet <fabet@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 07:52:46 by fabet             #+#    #+#             */
+/*   Updated: 2021/10/12 08:13:49 by fabet            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <unistd.h>
+
+static void	print_digits(int *n, int *size, int fd)
+{
+	char	c;
+
+	c = (*n / *size) + 48;
+	*n %= *size;
+	write(fd, &c, 1);
+	*size /= 10;
+}
+
+static void	print_zero(int n, int fd)
+{
+	char	c;
+
+	c = 0;
+	c = n + 48;
+	write(fd, &c, 1);
+}
+
+static void	print_minus(int *n, int fd)
+{
+	char	c;
+
+	c = '-';
+	write(fd, &c, 1);
+	*n = -*n;
+}
+
+static void	print_maxmin(int n, int fd)
+{
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	if (n == 2147483647)
+		ft_putstr_fd("2147483647", fd);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	int	size;
+	int	temp;
+
+	if (n == -2147483648 || n == 2147483647)
+	{
+		print_maxmin(n, fd);
+		return ;
+	}
+	size = 1;
+	if (n == 0)
+	{
+		print_zero(n, fd);
+		return ;
+	}
+	if (n < 0)
+		print_minus(&n, fd);
+	temp = n;
+	while (temp > 0)
+	{
+		temp /= 10;
+		size *= 10;
+	}
+	size /= 10;
+	while (size > 0)
+		print_digits(&n, &size, fd);
+}
