1a66ce8afe7e2b6ce1be103237fd7c591a663b99
fabet <asilyk112@gmail.com>
2022-06-28 19:39:58 +0300

    Working with mutexes

--- philo/ft_philosopher_routine.c
+++ philo/ft_philosopher_routine.c
@@ -6,7 +6,7 @@
 /*   By: alexander <alexander@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 22:57:36 by fabet             #+#    #+#             */
-/*   Updated: 2022/06/28 14:29:18 by alexander        ###   ########.fr       */
+/*   Updated: 2022/06/28 19:03:33 by alexander        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ static void	ft_take_fork(t_philosopher *philosopher, pthread_mutex_t *fork)
 	struct timeval	actual_time;
 
 	pthread_mutex_lock(fork);
-	pthread_mutex_lock(philosopher->output);
+
 	gettimeofday(&actual_time, NULL);
+
+	pthread_mutex_lock(philosopher->output);
 	printf("%ld %d has taken a fork\n", ft_count_timestamp_in_ms(philosopher->simulation_data->start_time, actual_time), philosopher->id);
 	pthread_mutex_unlock(philosopher->output);
 }
@@ -41,13 +43,16 @@ static void	ft_take_forks(t_philosopher *philosopher)
 
 static void	ft_eat(t_philosopher *philosopher)
 {
-	pthread_mutex_lock(philosopher->output);
 	pthread_mutex_lock(&philosopher->data_mutex);
 	gettimeofday(&philosopher->last_meal_time, NULL);
 	pthread_mutex_unlock(&philosopher->data_mutex);
+
+	pthread_mutex_lock(philosopher->output);
 	printf("%ld %d is eating\n", ft_count_timestamp_in_ms(philosopher->simulation_data->start_time, philosopher->last_meal_time), philosopher->id);
 	pthread_mutex_unlock(philosopher->output);
+
 	ft_sleep(philosopher->simulation_data->time_to_eat);
+
 	pthread_mutex_unlock(philosopher->right_fork);
 	pthread_mutex_unlock(philosopher->left_fork);
 }
@@ -56,10 +61,12 @@ static void	ft_fall_asleep(t_philosopher *philosopher)
 {
 	struct timeval	actual_time;
 
-	pthread_mutex_lock(philosopher->output);
 	gettimeofday(&actual_time, NULL);
+
+	pthread_mutex_lock(philosopher->output);
 	printf("%ld %d is sleeping\n", ft_count_timestamp_in_ms(philosopher->simulation_data->start_time, actual_time), philosopher->id);
 	pthread_mutex_unlock(philosopher->output);
+
 	ft_sleep(philosopher->simulation_data->time_to_sleep);
 }
 
@@ -67,8 +74,9 @@ static void ft_think(t_philosopher *philosopher)
 {
 	struct timeval	actual_time;
 
-	pthread_mutex_lock(philosopher->output);
 	gettimeofday(&actual_time, NULL);
+
+	pthread_mutex_lock(philosopher->output);
 	printf("%ld %d is thinking\n", ft_count_timestamp_in_ms(philosopher->simulation_data->start_time, actual_time), philosopher->id);
 	pthread_mutex_unlock(philosopher->output);
 }
@@ -83,29 +91,57 @@ void	*ft_philosopher_routine(void *data)
 		ft_sleep(philosopher->simulation_data->time_to_eat);
 	while (1)
 	{
-		if (philosopher->simulation_data->is_stopped == 0)
-			ft_take_forks(philosopher);
-		else
-			break;
+		pthread_mutex_lock(&philosopher->data_mutex);
 		if (philosopher->simulation_data->is_stopped == 0)
 		{
+			pthread_mutex_unlock(&philosopher->data_mutex);
+			ft_take_forks(philosopher);
+		}
+		else
+		{
+			pthread_mutex_unlock(&philosopher->data_mutex);
+			break;
+		}
+
+		pthread_mutex_lock(&philosopher->data_mutex);
+		if (philosopher->simulation_data->is_stopped == 0)
+		{
+			pthread_mutex_unlock(&philosopher->data_mutex);
 			ft_eat(philosopher);
 			pthread_mutex_lock(&philosopher->data_mutex);
 			philosopher->meals_count++;
 			pthread_mutex_unlock(&philosopher->data_mutex);
 		}
 		else
+		{
+			pthread_mutex_unlock(&philosopher->data_mutex);
 			break;
+		}
+
+		pthread_mutex_lock(&philosopher->data_mutex);
 		if (philosopher->simulation_data->is_stopped == 0)
+		{
 			ft_fall_asleep(philosopher);
+			pthread_mutex_unlock(&philosopher->data_mutex);
+		}
 		else
+		{
+			pthread_mutex_unlock(&philosopher->data_mutex);
 			break;
+		}
+
+
+		pthread_mutex_lock(&philosopher->data_mutex);
 		if (philosopher->simulation_data->is_stopped == 0)
+		{
+			pthread_mutex_unlock(&philosopher->data_mutex);
 			ft_think(philosopher);
+		}
 		else
+		{
+			pthread_mutex_unlock(&philosopher->data_mutex);
 			break;
-		// if (philosopher->simulation_data->is_stopped == 0)
-		// 	break;
+		}
 	}
 	return (NULL);
 }
--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: alexander <alexander@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 17:34:48 by fabet             #+#    #+#             */
-/*   Updated: 2022/06/28 14:16:26 by alexander        ###   ########.fr       */
+/*   Updated: 2022/06/28 19:38:38 by alexander        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,17 +53,26 @@ int	main(int argc, char *argv[])
 	int	i;
 	int	fed_philosophers;
 	struct timeval	actual_time;
+
 	while(1)
 	{
 		i = 0;
+		fed_philosophers = 0;
 		while(i < simulation_data->number_of_philosophers)
 		{
+			printf("WAIT MUTEX OF %d...\n", i);
 			pthread_mutex_lock(&philosophers[i].data_mutex);
+			printf("GET MUTEX OF %d!\n", i);
+
 			gettimeofday(&actual_time, NULL);
 			if (ft_count_timestamp_in_ms(philosophers[i].last_meal_time, actual_time) > simulation_data->time_to_die)
 			{
 				simulation_data->is_stopped = 1;
+
+				pthread_mutex_lock(&output);
 				printf("%ld %d died\n", ft_count_timestamp_in_ms(simulation_data->start_time, actual_time), philosophers[i].id);
+				pthread_mutex_lock(&output);
+
 				return (1);
 			}
 			if (simulation_data->number_of_meals > 0)
@@ -71,22 +80,26 @@ int	main(int argc, char *argv[])
 				if (philosophers[i].meals_count >= simulation_data->number_of_meals)
 					fed_philosophers++;
 			}
-			pthread_mutex_unlock(&philosophers[i].data_mutex);
 			i++;
+
+			pthread_mutex_unlock(&philosophers[i].data_mutex);
 		}
+
 		if (fed_philosophers == simulation_data->number_of_philosophers)
 		{
+			pthread_mutex_lock(&philosophers[i].data_mutex);
 			simulation_data->is_stopped = 1;
+			pthread_mutex_unlock(&philosophers[i].data_mutex);
 			break;
 		}
-		else
-			fed_philosophers = 0;
 	}
+
 	if (ft_join_threads(philosophers_pthreads, simulation_data) != 0)
 	{
 		//ft_free(simulation_data, philosophers_pthreads, forks, &output, philosophers);
 		return (1);
 	}
 	//ft_free(simulation_data, philosophers_pthreads, forks, &output, philosophers);
+
 	return (0);
 }
