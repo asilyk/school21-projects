0ca41fd377551d7b3d4170f9ef44e3fa7acbcbba
Ferret Abet <fabet@at-e6.msk.21-school.ru>
2022-07-01 13:42:09 +0300

    Restore files

---
+++ philo/ft_threads.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_threads.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fabet <fabet@student.21-school.ru>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/28 20:24:13 by fabet             #+#    #+#             */
+/*   Updated: 2022/07/01 13:23:56 by fabet            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_create_threads(
+		pthread_t *philos_pthreads,
+		t_philo *philos,
+		t_sim_data *sim_data)
+{
+	int	i;
+
+	i = 0;
+	while (i < sim_data->number_of_philos)
+	{
+		if (pthread_create(&philos_pthreads[i], NULL, &ft_philo_routine, &philos[i]) != 0)
+		{
+			printf("Error! Failed to create thread!\n");
+			return (1);
+		}
+		i++;
+	}
+	return(0);
+}
+
+int	ft_join_threads(pthread_t *philos_pthreads, t_sim_data *sim_data)
+{
+	int	i;
+
+	i = 0;
+	while (i < sim_data->number_of_philos)
+	{
+		if (pthread_join(philos_pthreads[i], NULL) != 0)
+		{
+			printf("Error! Failed to join thread!\n");
+			return (1);
+		}
+		i++;
+	}
+	return(0);
+}
