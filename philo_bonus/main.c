018b0486bf6e1eec87eef64728b0863182fc7866
Ferret Abet <fabet@at-f2.msk.21-school.ru>
2022-07-04 14:17:49 +0300

    Code refactoring

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -6,7 +6,7 @@
 /*   By: fabet <fabet@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 12:33:15 by fabet             #+#    #+#             */
-/*   Updated: 2022/07/04 13:59:38 by fabet            ###   ########.fr       */
+/*   Updated: 2022/07/04 14:16:44 by fabet            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(int argc, char **argv)
 
 	if (ft_parse_argv(&sim_data, argc, argv) == ERROR)
 		return (ERROR);
-	i = -1;
-	while (++i < sim_data.number_of_philos)
+	i = 0;
+	while (i < sim_data.number_of_philos)
 	{
 		sim_data.philos[i].pid = fork();
 		if (sim_data.philos[i].pid < 0)
@@ -28,6 +28,7 @@ int	main(int argc, char **argv)
 		if (sim_data.philos[i].pid == 0)
 			ft_philo_routine(&(sim_data.philos[i]));
 		usleep(100);
+		i++;
 	}
 	ft_stop_sim(&sim_data);
 	return (OK);
