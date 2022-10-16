561a33c499d78809b1277c60c282d19044b897bb
Ferret Abet <fabet@oa-i4.msk.21-school.ru>
2022-10-16 13:54:38 +0300

    Add ft_minishell_loop function

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: fabet <fabet@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 18:40:10 by fabet             #+#    #+#             */
-/*   Updated: 2022/10/15 19:16:16 by fabet            ###   ########.fr       */
+/*   Updated: 2022/10/16 13:45:33 by fabet            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,7 @@
 # include "utilities.h"
 # include "parser.h"
 # include "structures.h"
+
+int	ft_minishell_loop(t_tools *tools);
 
 #endif
--- minishell/sources/ft_minishell_loop.c
+++ minishell/sources/ft_minishell_loop.c
@@ -1,32 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_minishell_loop.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fabet <fabet@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/19 18:30:13 by fabet             #+#    #+#             */
-/*   Updated: 2022/10/16 13:44:40 by fabet            ###   ########.fr       */
+/*   Created: 2022/08/22 20:31:01 by fabet             #+#    #+#             */
+/*   Updated: 2022/10/16 13:53:54 by fabet            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_global	g_global;
-
-int	main(int argc, char **argv, char **envp)
+int	ft_minishell_loop(t_tools *tools)
 {
-	t_tools	tools;
+	char	*tmp;
 
-	(void)argv;
-	if (argc != 1)
+	tools->args = readline("ms$ ");
+	tmp = ft_strtrim(tools->args, " ");
+	free(tools->args);
+	tools->args = tmp;
+	if (!tools->args)
 	{
-		printf("Error! Invalid number of arguments.\n");
-		exit(0);
+		printf("wtf");
+		ft_putendl_fd("exit", STDOUT_FILENO);
+		exit(EXIT_SUCCESS);
 	}
-	tools.envp = ft_arrdup(envp);
-	ft_find_pwd(&tools);
-	ft_init_tools(&tools);
-	ft_minishell_loop(&tools);
-	return (0);
+	printf("ARGS: %s |\n", tools->args);
+	return (1);
 }
