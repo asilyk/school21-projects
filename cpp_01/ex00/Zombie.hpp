0b578f4b49c522c1d641ec0d382b680733693723
fabet <asilyk112@gmail.com>
2022-10-30 04:25:55 +0300

    Update ex00

--- cpp_01/ex00/Zombie.hpp
+++ cpp_01/ex00/Zombie.hpp
@@ -6,9 +6,12 @@
 /*   By: fabet <fabet@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 15:38:12 by fabet             #+#    #+#             */
-/*   Updated: 2022/10/29 15:56:41 by fabet            ###   ########.fr       */
+/*   Updated: 2022/10/30 04:21:19 by fabet            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
 
 # include <iostream>
 # include <string>
@@ -27,3 +30,5 @@ private:
 Zombie*	newZombie(std::string name);
 
 void randomChump(std::string name);
+
+#endif
